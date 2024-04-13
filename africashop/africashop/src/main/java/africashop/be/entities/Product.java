8d50bf02e541a84c27e4030fac2bfc9e078bc7e6
Denis <100221021+bidjdenis@users.noreply.github.com>
2024-04-13 14:25:22 +0200

    entities

---
+++ africashop/africashop/src/main/java/africashop/be/entities/Product.java
@@ -0,0 +1,11 @@
+package africashop.be.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "product")
+public class Product {
+}
