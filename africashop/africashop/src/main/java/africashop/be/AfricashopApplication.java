8f04f65b7619ef0973e3c10d18db4b546573508f
Denis <100221021+bidjdenis@users.noreply.github.com>
2024-04-13 14:24:49 +0200

    springboot adding

---
+++ africashop/africashop/src/main/java/africashop/be/AfricashopApplication.java
@@ -0,0 +1,13 @@
+package africashop.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfricashopApplication.class, args);
+	}
+
+}
